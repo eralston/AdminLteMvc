d54e87d083e059a3d05c96d46897a667592943d4
Erik Ralston <erik.ralston@gmail.com>
2015-04-12 17:47:38 -0700

    Initial ASP.Net MVC Web App with Admin LTE Dashboard integration

---
+++ AdminLteMvc/AdminLte/Breadcrumb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Crumb
+{
+    public Crumb() { }
+    public Crumb(string text)
+    {
+        Text = text;
+    }
+    public Crumb(string text, string link)
+        : this(text)
+    {
+        this.Link = link;
+    }
+    public Crumb(string text, string link, string icon)
+        : this(text)
+    {
+        this.Link = link;
+        this.Icon = icon;
+    }
+
+    public string Text { get; set; }
+    public bool HasText { get { return !string.IsNullOrEmpty(Text); } }
+
+    public string Link { get; set; }
+    public bool HasLink { get { return !string.IsNullOrEmpty(Link); } }
+
+    public string Icon { get; set; }
+    public bool HasIcon { get { return !string.IsNullOrEmpty(Icon); } }
+}
+
+public class Breadcrumb : List<Crumb>
+{
+    public Breadcrumb(IEnumerable<Crumb> crumbs) : base(crumbs) { }
+}
---
+++ AdminLteMvc/AdminLteMvc/Controllers/AdminLteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminLteMvc.Controllers
+{
+    public class AdminLteController : Controller
+    {
+        // GET: AdminLte
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
