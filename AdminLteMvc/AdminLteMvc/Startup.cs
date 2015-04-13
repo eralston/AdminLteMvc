c010ebf7eb4c338f1231e00475ecd3a73e28106c
Erik Ralston <erik.ralston@gmail.com>
2015-04-12 17:50:24 -0700

    Additional files for the demo app

---
+++ AdminLteMvc/AdminLteMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminLteMvc.Startup))]
+namespace AdminLteMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
